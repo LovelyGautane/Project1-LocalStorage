d12a3068bb1ac3ee5c72f08a74e9894d7b581998
LovelyGautane <gautanelovely96@gmail.com>
2021-05-01 03:22:04 +0800

    Adding new elements into the DOM

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 // Variables
-
+    const tweetList = document.getElementById('tweet-list');
 
 
   // Event Listener 
@@ -16,5 +16,21 @@
   function newTweet(e) {
        e.preventDefault();
 
-       console.log('Form Submitted');
+         //Read the textarea value
+         const tweet = document.getElementById('tweet').value;
+
+         // Create the remove button
+        const removeBtn = document.createElement('a');
+        removeBtn.classList = 'remove-tweet';
+        removeBtn.textContent = 'X';
+
+        // Create an <li> element
+        const li = document.createElement('li');
+        li.textContent = tweet;
+
+         // Add the remove button to each tweet
+         li.appendChild(removeBtn);
+         
+        // Add to the list
+         tweetList.appendChild(li);
 }
